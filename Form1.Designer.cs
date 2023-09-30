b76fb489d5ce8d8112c0809b258615f9e121c36d
kenth <kenth@LAPTOP-832N4QBD>
2023-09-30 23:56:04 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TicTacToe {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnTile1 = new System.Windows.Forms.Button();
+            this.btnTile2 = new System.Windows.Forms.Button();
+            this.btnTile3 = new System.Windows.Forms.Button();
+            this.btnTile4 = new System.Windows.Forms.Button();
+            this.btnTile5 = new System.Windows.Forms.Button();
+            this.btnTile6 = new System.Windows.Forms.Button();
+            this.btnTile7 = new System.Windows.Forms.Button();
+            this.btnTile8 = new System.Windows.Forms.Button();
+            this.btnTile9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTile1
+            // 
+            this.btnTile1.Location = new System.Drawing.Point(293, 155);
+            this.btnTile1.Name = "btnTile1";
+            this.btnTile1.Size = new System.Drawing.Size(150, 150);
+            this.btnTile1.TabIndex = 0;
+            this.btnTile1.UseVisualStyleBackColor = true;
+            // 
+            // btnTile2
+            // 
+            this.btnTile2.Location = new System.Drawing.Point(540, 155);
+            this.btnTile2.Name = "btnTile2";
+            this.btnTile2.Size = new System.Drawing.Size(150, 150);
+            this.btnTile2.TabIndex = 1;
+            this.btnTile2.UseVisualStyleBackColor = true;
+            // 
+            // btnTile3
+            // 
+            this.btnTile3.Location = new System.Drawing.Point(786, 155);
+            this.btnTile3.Name = "btnTile3";
+            this.btnTile3.Size = new System.Drawing.Size(150, 150);
+            this.btnTile3.TabIndex = 2;
+            this.btnTile3.UseVisualStyleBackColor = true;
+            // 
+            // btnTile4
+            // 
+            this.btnTile4.Location = new System.Drawing.Point(293, 373);
+            this.btnTile4.Name = "btnTile4";
+            this.btnTile4.Size = new System.Drawing.Size(150, 150);
+            this.btnTile4.TabIndex = 3;
+            this.btnTile4.UseVisualStyleBackColor = true;
+            // 
+            // btnTile5
+            // 
+            this.btnTile5.Location = new System.Drawing.Point(540, 373);
+            this.btnTile5.Name = "btnTile5";
+            this.btnTile5.Size = new System.Drawing.Size(150, 150);
+            this.btnTile5.TabIndex = 4;
+            this.btnTile5.UseVisualStyleBackColor = true;
+            // 
+            // btnTile6
+            // 
+            this.btnTile6.Location = new System.Drawing.Point(786, 373);
+            this.btnTile6.Name = "btnTile6";
+            this.btnTile6.Size = new System.Drawing.Size(150, 150);
+            this.btnTile6.TabIndex = 5;
+            this.btnTile6.UseVisualStyleBackColor = true;
+            // 
+            // btnTile7
+            // 
+            this.btnTile7.Location = new System.Drawing.Point(293, 588);
+            this.btnTile7.Name = "btnTile7";
+            this.btnTile7.Size = new System.Drawing.Size(150, 150);
+            this.btnTile7.TabIndex = 6;
+            this.btnTile7.UseVisualStyleBackColor = true;
+            // 
+            // btnTile8
+            // 
+            this.btnTile8.Location = new System.Drawing.Point(540, 588);
+            this.btnTile8.Name = "btnTile8";
+            this.btnTile8.Size = new System.Drawing.Size(150, 150);
+            this.btnTile8.TabIndex = 7;
+            this.btnTile8.UseVisualStyleBackColor = true;
+            // 
+            // btnTile9
+            // 
+            this.btnTile9.Location = new System.Drawing.Point(786, 588);
+            this.btnTile9.Name = "btnTile9";
+            this.btnTile9.Size = new System.Drawing.Size(150, 150);
+            this.btnTile9.TabIndex = 8;
+            this.btnTile9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 837);
+            this.Controls.Add(this.btnTile9);
+            this.Controls.Add(this.btnTile8);
+            this.Controls.Add(this.btnTile7);
+            this.Controls.Add(this.btnTile6);
+            this.Controls.Add(this.btnTile5);
+            this.Controls.Add(this.btnTile4);
+            this.Controls.Add(this.btnTile3);
+            this.Controls.Add(this.btnTile2);
+            this.Controls.Add(this.btnTile1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTile1;
+        private System.Windows.Forms.Button btnTile2;
+        private System.Windows.Forms.Button btnTile3;
+        private System.Windows.Forms.Button btnTile4;
+        private System.Windows.Forms.Button btnTile5;
+        private System.Windows.Forms.Button btnTile6;
+        private System.Windows.Forms.Button btnTile7;
+        private System.Windows.Forms.Button btnTile8;
+        private System.Windows.Forms.Button btnTile9;
+    }
+}
+
